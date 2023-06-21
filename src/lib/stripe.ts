050e4bd3ef0a3b474b3ed5fa034b83a40aa7fdeb
Mariana Castro <marianacastrorc@gmail.com>
2023-06-21 17:53:30 -0300

    Refactoring code

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,6 +4,6 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY ?? ''
 export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2022-11-15',
   appInfo: {
-    name: 'Ignite Shop',
+    name: 'Atomic Shop',
   },
 })
